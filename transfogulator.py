c4ab8b7e14b91d7d652c730aa05bda90a76708fb
gadzook <ivysly7@gmail.com>
2017-02-18 14:37:54 -0800

    Update transfogulator.py

--- transfogulator.py
+++ transfogulator.py
@@ -5,8 +5,8 @@ def transfogulate(str_list):
 	Simply iterates between each letter in all strings, sifting them together 
 	into one long one.
 
-	For example, running this function with the parameters "Hello World!" and 
-	"O Shit Waddup" returns the string "HOe lSlhoi tW oWralddd!up".
+	For example, running this function with the parameters "AAAAA" and 
+	"BBBBB" returns the string "ABABABABAB".
 
 	Once it reaches the end of the shortest string, it will just append the
 	rest of the longer ones' letters.
